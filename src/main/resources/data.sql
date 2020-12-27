b5a55ab6594ab08fe319a58b27d26643c384071b
amit <amdel2020@gmail.com>
2020-12-27 23:38:39 +0530

    Added data.sql file for inserting sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO category (name) VALUES ('Fruits');
+INSERT INTO category (name) VALUES ('Dried');
+INSERT INTO category (name) VALUES ('Exotic');
+INSERT INTO category (name) VALUES ('Nuts');
